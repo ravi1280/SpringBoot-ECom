7d0992d48e61098b47e5459d4a4044e00308973f
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-09-24 17:07:20 +0530

    Initialize Spring Boot E-Commerce project
    Set up a new Spring Boot project with Maven wrapper, basic configuration files, and initial application class. Includes dependencies for web, JPA, actuator, Lombok, MySQL, and test support.

---
+++ E-Commerce/src/main/java/com/development/ecom/upscale/UpscaleApplication.java
@@ -0,0 +1,13 @@
+package com.development.ecom.upscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpscaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpscaleApplication.class, args);
+	}
+
+}
